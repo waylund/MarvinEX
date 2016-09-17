3de27af18a7b9e955b0a70e067283d3a51f8a1b7
Daniel Lynn <dlynn@amalgam-it.com>
2016-09-16 19:48:46 -0600

    Base Exercise with all tests running

---
+++ MarvinEX/Marvin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvinEX
+{
+    public class Marvin
+    {
+        public string talk(string statement)
+        {
+            if (String.IsNullOrWhiteSpace(statement))
+                return "Fine. Don't say anything.";
+            if (statement.ToUpper().Equals(statement))
+                return "No need to yell. I'm right here.";
+            if (statement.Substring(statement.Length-1, 1).Equals("?"))
+                return "I don't care.";
+            return "Whatever.";
+        }
+    }
+}
---
+++ MarvinEXTests/MarvinUnitTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarvinEX;
+
+namespace MarvinEXTests
+{
+    [TestClass]
+    public class MarvinUnitTests
+    {
+        [TestMethod]
+        public void GenericStatementTest()
+        {
+            //Arrange
+            Marvin marvin = new Marvin();
+
+            //Act
+            string response = marvin.talk("Hi there.");
+
+            //Assert
+            Assert.AreEqual("Whatever.", response);
+        }
+
+        [TestMethod]
+        public void AskAQuestionTest()
+        {
+            Marvin marvin = new Marvin();
+            string response = marvin.talk("How are you?");
+            Assert.AreEqual("I don't care.", response);
+        }
+
+        [TestMethod]
+        public void StayingQuietTest()
+        {
+            Marvin marvin = new Marvin();
+            string response = marvin.talk(" ");
+            Assert.AreEqual("Fine. Don't say anything.", response);
+        }
+
+        [TestMethod]
+        public void StayingQuietEmptyStringTest()
+        {
+            Marvin marvin = new Marvin();
+            string response = marvin.talk("");
+            Assert.AreEqual("Fine. Don't say anything.", response);
+        }
+
+        [TestMethod]
+        public void DontYellTest()
+        {
+            Marvin marvin = new Marvin();
+            string response = marvin.talk("DON'T TOUCH THAT!");
+            Assert.AreEqual("No need to yell. I'm right here.", response);
+        }
+
+        [TestMethod]
+        public void YellingTrumpsQuestionTest()
+        {
+            Marvin marvin = new Marvin();
+            string response = marvin.talk("WHAT IS IT MAN?");
+            Assert.AreEqual("No need to yell. I'm right here.", response);
+        }
+    }
+}
